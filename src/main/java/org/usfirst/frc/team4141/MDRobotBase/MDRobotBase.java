a7784d1aea334e87fa122e330b0f848cc27eb60b
Joe Frayne <joefrayne@gmail.com>
2019-01-14 21:54:00 -0800

    Switched deprecated CameraServer to new package
    http://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/wpilibj/package-summary.html

--- src/main/java/org/usfirst/frc/team4141/MDRobotBase/MDRobotBase.java
+++ src/main/java/org/usfirst/frc/team4141/MDRobotBase/MDRobotBase.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 //import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
