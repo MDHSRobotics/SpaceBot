0c28388393b9459c8a9041b6bccb35bb83369ba7
Joe Frayne <joefrayne@gmail.com>
2019-02-19 14:34:05 -0800

    Progress with figuring out RioLog colors

--- src/main/java/frc/robot/consoles/Logger.java
+++ src/main/java/frc/robot/consoles/Logger.java
@@ -1,9 +1,6 @@
 
 package frc.robot.consoles;
 
-import com.sun.jna.*;
-import com.sun.jna.platform.win32.WinDef.*;
-import com.sun.jna.platform.win32.WinNT.HANDLE;
 import edu.wpi.first.wpilibj.DriverStation;
 
 
@@ -23,31 +20,13 @@ public class Logger {
     public static final String ANSI_WHITE = "\u001B[37m";
 
     public Logger() {
-        // Support ANSI colors on Windows 10 consoles
-        // https://stackoverflow.com/questions/52767585/how-can-you-use-vt100-escape-codes-in-java-on-windows
-        String osName = System.getProperty("os.name");
-        System.out.println("Current OS: " + osName);
-        if (osName.startsWith("Windows")) {
-            System.out.println("Windows OS - Attempting to support VT100...");
+        // TODO: See if we can implement support for this in our WPILib VSCode Extension
+        // This is the file that is actively used by VSCode:
+        // C:\Users\Public\frc2019\vscode\data\extensions\wpilibsuite.vscode-wpilib-2019.2.1\resources\dist\riologpage.js
 
-            // Set output mode to handle virtual terminal sequences
-            Function GetStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");
-            DWORD STD_OUTPUT_HANDLE = new DWORD(-11);
-            HANDLE hOut = (HANDLE)GetStdHandleFunc.invoke(HANDLE.class, new Object[]{STD_OUTPUT_HANDLE});
-
-            DWORDByReference p_dwMode = new DWORDByReference(new DWORD(0));
-            Function GetConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");
-            GetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, p_dwMode});
-
-            int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;
-            DWORD dwMode = p_dwMode.getValue();
-            dwMode.setValue(dwMode.intValue() | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-            Function SetConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");
-            SetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, dwMode});
-        }
+        // This doesn't work yet
         System.out.println("If this is colorful, you can use ANSI colors in this terminal: \033[0;31m A \033[0;32m N \033[0;33m S \033[0;34m I \u001B[0m!");
         System.out.println("Or if this is colorful, you can use ANSI colors in this terminal: \u001B[31m A \u001B[32m N \u001B[33m S \u001B[34m I \033[0m!");
-        System.out.println("<font color=\"red\">This is red some text!</font>");
     }
 
     // Grey text for constructor logging, etc.
